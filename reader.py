6167b12425a27fe4925427059fc576a35297c4fc
Lab-Brat <xoovoox@protonmail.com>
2022-11-30 23:31:32 +0300

    show percentage

--- reader.py
+++ reader.py
@@ -4,4 +4,19 @@ from pprint import pprint
 with open('self.yaml', 'r') as file:
     yml = yaml.safe_load(file)
 
-pprint(yml)
+def show_results(yml):
+    for section in yml:
+        print(f"{section}:")
+        for task in yml[section]:
+            name = task['task']
+            result = len(task['status']) / task['times']
+            match result:
+                case 1.0:
+                    print(f'{name} ---> Done!')
+                case _:
+                    print(f'{name} ---> {result*100:.2f}% done')
+        print('\n')
+
+
+if __name__ == '__main__':
+    show_results(yml)
