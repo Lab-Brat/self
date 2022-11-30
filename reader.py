bd71107e5bbafd8abe057d06c0d6ac6837fc618d
Lab-Brat <xoovoox@protonmail.com>
2022-11-30 23:37:37 +0300

    format output better

--- reader.py
+++ reader.py
@@ -8,14 +8,18 @@ def show_results(yml):
     for section in yml:
         print(f"{section}:")
         for task in yml[section]:
-            name = task['task']
-            result = len(task['status']) / task['times']
-            match result:
-                case 1.0:
-                    print(f'{name} ---> Done!')
-                case _:
-                    print(f'{name} ---> {result*100:.2f}% done')
+            get_task_info(task)
         print('\n')
+
+def get_task_info(task):
+    name = task['task']
+    dash = (20 - len(name)) * '-'
+    result = len(task['status']) / task['times']
+    match result:
+        case 1.0:
+            print(f'{name} {dash}> Done!')
+        case _:
+            print(f'{name} {dash}> {result*100:.2f}% done')
 
 
 if __name__ == '__main__':
