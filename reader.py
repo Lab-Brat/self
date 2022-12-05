7b8afbb94f8f3311ab81f11d3f77efba05cf1925
Lab-Brat <xoovoox@protonmail.com>
2022-12-05 22:31:46 +0300

    add comments

--- reader.py
+++ reader.py
@@ -5,10 +5,17 @@ class SelfYAML:
         self.yml = self._read_yaml(yaml_file)
     
     def _read_yaml(self, yaml_file):
+        '''
+        Read self.yaml, return a dictionary
+        '''
         with open(yaml_file, 'r') as file:
             return yaml.safe_load(file)
 
     def _get_task_info(self, task):
+        '''
+        Get information about a task,
+        print the result.
+        '''
         name = task['task']
         dash = (20 - len(name)) * '-'
         result = len(task['status']) / task['times']
@@ -19,6 +26,9 @@ class SelfYAML:
                 print(f'{name} {dash}> {result*100:.2f}% done')
 
     def show_results(self):
+        '''
+        Get information about every task.
+        '''
         for section in self.yml:
             print(f"{section}:")
             for task in self.yml[section]:
