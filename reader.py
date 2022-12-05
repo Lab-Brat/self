dba3b98a7e493ac574d33bd82f6832b402438ba1
Lab-Brat <xoovoox@protonmail.com>
2022-12-05 22:23:05 +0300

    classify

--- reader.py
+++ reader.py
@@ -1,26 +1,31 @@
 import yaml
-from pprint import pprint
 
-with open('self.yaml', 'r') as file:
-    yml = yaml.safe_load(file)
+class SelfYAML:
+    def __init__(self, yaml_file):
+        self.yml = self._read_yaml(yaml_file)
+    
+    def _read_yaml(self, yaml_file):
+        with open(yaml_file, 'r') as file:
+            return yaml.safe_load(file)
 
-def show_results(yml):
-    for section in yml:
-        print(f"{section}:")
-        for task in yml[section]:
-            get_task_info(task)
-        print('\n')
+    def _get_task_info(self, task):
+        name = task['task']
+        dash = (20 - len(name)) * '-'
+        result = len(task['status']) / task['times']
+        match result:
+            case 1.0:
+                print(f'{name} {dash}> Done!')
+            case _:
+                print(f'{name} {dash}> {result*100:.2f}% done')
 
-def get_task_info(task):
-    name = task['task']
-    dash = (20 - len(name)) * '-'
-    result = len(task['status']) / task['times']
-    match result:
-        case 1.0:
-            print(f'{name} {dash}> Done!')
-        case _:
-            print(f'{name} {dash}> {result*100:.2f}% done')
+    def show_results(self):
+        for section in self.yml:
+            print(f"{section}:")
+            for task in self.yml[section]:
+                self._get_task_info(task)
+            print('\n')
 
 
 if __name__ == '__main__':
-    show_results(yml)
+    ya = SelfYAML('self.yaml')
+    ya.show_results()
